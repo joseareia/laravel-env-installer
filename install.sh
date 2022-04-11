5aaad98866ec2ab90c5d508bf637b6dae1004bfc
joseareia <jose.apareia@gmail.com>
2022-04-11 14:52:38 +0100

    edit: import nginx-config to the installation path

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ fi
 
 if [ -d "/opt/laravel-env-installer" ]; then
     sudo rm -r /opt/laravel-env-installer
+    sudo rm -r /opt/nginx-config
     sudo rm -r /usr/local/bin/laravel-env-installer
 fi
 
@@ -13,6 +14,7 @@ sudo mkdir /opt/laravel-env-installer
 
 echo -e "Copying the files to /opt/laravel-env-installer..."
 sudo cp --preserve laravel-env-installer.sh /opt/laravel-env-installer
+sudo cp --preserve nginx-config /opt/nginx-config
 
 echo -e "Creating a symlink of 'laravel-env-installer.sh' to /usr/local/bin/laravel-env-installer..."
 sudo ln -s /opt/laravel-env-installer/laravel-env-installer.sh /usr/local/bin/laravel-env-installer
